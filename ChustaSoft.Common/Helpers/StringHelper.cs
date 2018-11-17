8a2d0c629f6248ef0989fdd50e110796585e6434
Xavi Rueda Abelaira <xavi_rueda@outlook.com>
2018-11-17 18:51:03 +0100

    Added new methods for EnumsHelper: - GetByString - GetEnumList - GetEnumDictionary - Added missing documentation for String Exentesion methods from version 1.0.11

--- ChustaSoft.Common/Helpers/StringHelper.cs
+++ ChustaSoft.Common/Helpers/StringHelper.cs
@@ -10,6 +10,11 @@ namespace ChustaSoft.Common.Helpers
 
         #region Extension Methods
 
+        /// <summary>
+        /// Transform a string changing first letter to upper, and the rest to lower
+        /// </summary>
+        /// <param name="str">String itself to transform</param>
+        /// <returns>String transformed</returns>
         public static string ToUpperCamelCase(this string str)
         {
             CheckStringData(str);
@@ -17,6 +22,11 @@ namespace ChustaSoft.Common.Helpers
             return char.ToUpper(str.First()) + str.Substring(1).ToLower();
         }
 
+        /// <summary>
+        /// Transform a string changing just the first letter to upper
+        /// </summary>
+        /// <param name="str">String itself to transform</param>
+        /// <returns>String transformed</returns>
         public static string FirstToUpper(this string str)
         {
             CheckStringData(str);
@@ -24,6 +34,11 @@ namespace ChustaSoft.Common.Helpers
             return char.ToUpper(str.First()) + str.Substring(1);
         }
 
+        /// <summary>
+        /// Transform a string changing just the first letter to lower
+        /// </summary>
+        /// <param name="str">String itself to transform</param>
+        /// <returns>String transformed</returns>
         public static string FirstToLower(this string str)
         {
             CheckStringData(str);
