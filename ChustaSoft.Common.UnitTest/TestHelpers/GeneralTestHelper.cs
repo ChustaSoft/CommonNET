777fd1e9e35f9451cf51d5a7e6add831dedb4f1b
Xavi Rueda Abelaira <xavi_rueda@outlook.com>
2019-02-05 15:56:28 +0100

    ObjectHelper -> GetDescription method added

---
+++ ChustaSoft.Common.UnitTest/TestHelpers/GeneralTestHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+
+namespace ChustaSoft.Common.UnitTest.TestHelpers
+{    
+    internal enum TestEnum
+    {
+        WithoutDescription,
+
+        [Description("1")]
+        WithDescription1,
+
+        [Description("2")]
+        WithDescription2
+    }
+
+    internal class TestClass
+    {
+
+        [System.ComponentModel.Description("Test description")]
+        public string KnownDescription { get; set; }
+
+        public string UnknownDescription { get; set; }
+
+    }
+}
