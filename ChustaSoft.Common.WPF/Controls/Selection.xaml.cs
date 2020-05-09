a6c916b6addd257ff7c90855fc0de62020bef615
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2020-05-09 03:37:35 +0200

    Removed base class for Selection, not working on beta

--- ChustaSoft.Common.WPF/Controls/Selection.xaml.cs
+++ ChustaSoft.Common.WPF/Controls/Selection.xaml.cs
@@ -1,14 +1,34 @@
 ﻿using ChustaSoft.Common.Models;
+using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace ChustaSoft.Common.Controls
 {
-    public partial class Selection : SelectionBase
+    public partial class Selection : UserControl
     {
+
+        public static DependencyProperty DefaultTextProperty =
+            DependencyProperty.Register(nameof(DefaultText), typeof(string), typeof(Selection));
+
+        public static DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register(nameof(Items), typeof(IEnumerable<SelectableOption>), typeof(Selection));
 
         public static DependencyProperty SelectedItemProperty =
             DependencyProperty.Register(nameof(ItemSelected), typeof(SelectableOption), typeof(Selection));
 
+
+        public string DefaultText
+        {
+            get { return (string)GetValue(DefaultTextProperty); }
+            set { SetValue(DefaultTextProperty, value); }
+        }
+
+        public IEnumerable<SelectableOption> Items
+        {
+            get { return (IEnumerable<SelectableOption>)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
 
         public SelectableOption ItemSelected
         {
@@ -23,6 +43,4 @@ namespace ChustaSoft.Common.Controls
         }
 
     }
-
-    public class SelectionBase : SelectionControlBase<Selection> { }
 }
