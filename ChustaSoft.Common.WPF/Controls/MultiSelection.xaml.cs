66c984135e5c430caa34d26bbea7352c61c6f568
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2020-05-09 02:56:40 +0200

    Base class added for Selection controls

--- ChustaSoft.Common.WPF/Controls/MultiSelection.xaml.cs
+++ ChustaSoft.Common.WPF/Controls/MultiSelection.xaml.cs
@@ -1,32 +1,7 @@
-﻿using ChustaSoft.Common.Models;
-using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Controls;
-
-namespace ChustaSoft.Common.Controls
+﻿namespace ChustaSoft.Common.Controls
 {
-    public partial class MultiSelection : UserControl
+    public partial class MultiSelection : MultiSelectionBase
     {
-
-        public static DependencyProperty DefaultTextProperty =
-            DependencyProperty.Register(nameof(DefaultText), typeof(string), typeof(MultiSelection));
-
-        public static DependencyProperty ItemsSourceProperty =
-            DependencyProperty.Register(nameof(Items), typeof(IEnumerable<SelectableOption>), typeof(MultiSelection));
-
-
-        public string DefaultText
-        {
-            get { return (string)GetValue(DefaultTextProperty); }
-            set { SetValue(DefaultTextProperty, value); }
-        }
-
-        public IEnumerable<SelectableOption> Items
-        {
-            get { return (IEnumerable<SelectableOption>)GetValue(ItemsSourceProperty); }
-            set { SetValue(ItemsSourceProperty, value); }
-        }
-
 
         public MultiSelection()
         {
@@ -34,4 +9,8 @@ namespace ChustaSoft.Common.Controls
         }
 
     }
+
+
+    public class MultiSelectionBase : SelectionControlBase<MultiSelection> { }
+
 }
