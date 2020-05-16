af852fef9c1f5034dc6a8102419ca9482798b10a
Victor <diblix69@hotmail.com>
2020-05-16 19:39:10 +0200

    Add Loading control

--- ChustaSoft.Common.WPF/Controls/Loading.xaml.cs
+++ ChustaSoft.Common.WPF/Controls/Loading.xaml.cs
@@ -1,21 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ChustaSoft.Common.Controls
 {
-    /// <summary>
-    /// Lógica de interacción para Loading.xaml
-    /// </summary>
     public partial class Loading : UserControl
     {
         public Loading()
