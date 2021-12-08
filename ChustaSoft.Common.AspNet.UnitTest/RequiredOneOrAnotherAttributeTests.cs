745e87ad69e6b436569ae75f1cd955efd458c5b4
Xelit3 <xavi_abelaira@outlook.com>
2021-12-08 09:56:56 +0100

    RequiredOneOrAnotherAttribute validation attribute added

---
+++ ChustaSoft.Common.AspNet.UnitTest/RequiredOneOrAnotherAttributeTests.cs
@@ -0,0 +1,77 @@
+﻿using ChustaSoft.Common.Validators;
+using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChustaSoft.Common.AspNet.UnitTest
+{
+    public class RequiredOneOrAnotherAttributeTests
+    {
+
+        [Test]
+        public void Given_PropertiesRelatedInitialInformed_When_IsValidBypass_Then_NullRetrieved()
+        {
+            var classObj = new TestClass { TestString = "Test value" };
+            var validator = new RequiredOneOrAnotherAttributeTestObj(nameof(TestClass.TestInt));
+            var validationContext = new ValidationContext(classObj) { MemberName = nameof(TestClass.TestString) };
+
+            var result = validator._IsValid(classObj, validationContext);
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void Given_PropertiesRelatedOtherInformed_When_IsValidBypass_Then_NullRetrieved()
+        {
+            var classObj = new TestClass { TestInt = 5 };
+            var validator = new RequiredOneOrAnotherAttributeTestObj(nameof(TestClass.TestString));
+            var validationContext = new ValidationContext(classObj) { MemberName = nameof(TestClass.TestInt) };
+
+            var result = validator._IsValid(classObj, validationContext);
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void Given_PropertiesRelatedNoneInformed_When_IsValidBypass_Then_ErrorMessageRetrieved()
+        {
+            var classObj = new TestClass { };
+            var validator = new RequiredOneOrAnotherAttributeTestObj(nameof(TestClass.TestString));
+            var validationContext = new ValidationContext(classObj) { MemberName = nameof(TestClass.TestInt) };
+
+            var result = validator._IsValid(classObj, validationContext);
+
+            Assert.IsNotNull(result.ErrorMessage);
+        }
+
+
+        [Test]
+        public void Given_PropertiesRelatedBothInformed_When_IsValidBypass_Then_NullRetrieved()
+        {
+            var classObj = new TestClass { TestString = "Test", TestInt = 5 };
+            var validator = new RequiredOneOrAnotherAttributeTestObj(nameof(TestClass.TestString));
+            var validationContext = new ValidationContext(classObj) { MemberName = nameof(TestClass.TestInt) };
+
+            var result = validator._IsValid(classObj, validationContext);
+
+            Assert.IsNull(result);
+        }
+
+
+
+        private class RequiredOneOrAnotherAttributeTestObj : RequiredOneOrAnotherAttribute
+        {
+            public RequiredOneOrAnotherAttributeTestObj(string otherProperty) 
+                : base(otherProperty)
+            { }
+
+            internal ValidationResult _IsValid(object value, ValidationContext validationContext)
+                => base.IsValid(value, validationContext);
+        }
+
+        private class TestClass
+        {
+            public string TestString { get; set; }
+            public int TestInt { get; set; }
+        }
+    }
+}
---
+++ ChustaSoft.Common.AspNet/Validators/RequiredOneOrAnotherAttribute.cs
@@ -0,0 +1,59 @@
+﻿using ChustaSoft.Common.Helpers;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChustaSoft.Common.Validators
+{
+    public class RequiredOneOrAnotherAttribute : ValidationAttribute, IClientModelValidator
+    {
+
+        private readonly string _otherProperty;
+
+
+        public RequiredOneOrAnotherAttribute(string otherProperty)
+        {
+            _otherProperty = otherProperty;
+        }
+
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var property1Value = value.GetPropertyValue(validationContext.MemberName);
+            var property2Value = value.GetPropertyValue(_otherProperty);
+
+            if (!IsPropertyTypeValid(property1Value) && !IsPropertyTypeValid(property2Value))
+            {
+                var property1Description = validationContext.ObjectType.GetDescription(validationContext.MemberName);
+                var property2Description = validationContext.ObjectType.GetDescription(_otherProperty);
+
+                return new ValidationResult($"Field {property1Description} or {property2Description} are mandatory");
+            }
+
+            return null;
+        }
+
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            const string validationKey = "data-val-oneoranother";
+            if (context.Attributes.ContainsKey(validationKey))
+                context.Attributes.Add(validationKey, $"Property required");
+        }
+
+        private bool IsPropertyTypeValid(object property) 
+        {
+            if (property == null)
+                return false;
+
+            switch (property.GetType().Name)
+            {
+                case "Int32":
+                    return !((int)property == 0);
+
+                case "string":
+                default:
+                    return !string.IsNullOrEmpty(property as string);
+            }
+        }
+
+    }
+}
