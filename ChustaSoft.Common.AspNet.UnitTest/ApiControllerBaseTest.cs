bf628403874aa115d35cdae86625a63863b09c50
Xelit3 <xavi_abelaira@outlook.com>
2022-04-27 20:18:00 +0200

    Added new cases in GetRequestUserEmail

--- ChustaSoft.Common.AspNet.UnitTest/ApiControllerBaseTest.cs
+++ ChustaSoft.Common.AspNet.UnitTest/ApiControllerBaseTest.cs
@@ -1,12 +1,14 @@
 using ChustaSoft.Common.Base;
 using ChustaSoft.Common.Builders;
 using ChustaSoft.Common.Utilities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
 using System;
 using System.Net;
+using System.Security.Claims;
 
 namespace ChustaSoft.Common.AspNet.UnitTest
 {
@@ -56,10 +58,9 @@ namespace ChustaSoft.Common.AspNet.UnitTest
         {
             //Arrange
             const string expectedUserId = "ff85452f-465b-4539-a056-fd516d635df5";
-            var httpContext = CreateHttpContextWithAuthrizationHeader();
             var testController = CreateTestController(new ControllerContext()
             {
-                HttpContext = httpContext,
+                HttpContext = CreateHttpContextWithAuthrizationHeader(),
             });
 
             //Act
@@ -75,10 +76,9 @@ namespace ChustaSoft.Common.AspNet.UnitTest
             //Arrange
             const string expectedErrorMessage = "User id cannot be null";
 
-            var httpContext = CreateHttpContextWithAuthrizationHeader();
             var testController = CreateTestController(new ControllerContext()
             {
-                HttpContext = httpContext,
+                HttpContext = CreateHttpContextWithAuthrizationHeader(),
             });
 
             //Act
@@ -93,10 +93,9 @@ namespace ChustaSoft.Common.AspNet.UnitTest
         public void Given_TokenWithEmail_When_GetRequestUserEmail_Then_UserEmailRetrived()
         {
             //Arrange
-            var httpContext = CreateHttpContextWithAuthrizationHeader();
             var testController = CreateTestController(new ControllerContext()
             {
-                HttpContext = httpContext,
+                HttpContext = CreateHttpContextWithAuthrizationHeader(),
             });
 
             //Act
@@ -106,6 +105,55 @@ namespace ChustaSoft.Common.AspNet.UnitTest
             Assert.IsFalse(string.IsNullOrWhiteSpace(userEmail));
         }
 
+        [Test]
+        public void Given_IdentityWithEmail_When_GetRequestUserEmail_Then_UserEmailRetrived()
+        {
+            //Arrange
+            var testController = CreateTestController(new ControllerContext()
+            {
+                HttpContext = CreateHttpContextWithIdentity(),
+            });
+
+            //Act
+            var userEmail = testController.GetRequestUserEmail();
+
+            //Assert
+            Assert.IsFalse(string.IsNullOrWhiteSpace(userEmail));
+        }
+
+        [Test]
+        public void Given_WithEmailInUpnClaim_When_GetRequestUserEmail_Then_UserEmailRetrived()
+        {
+            //Arrange
+            var testController = CreateTestController(new ControllerContext()
+            {
+                HttpContext = CreateHttpContextWithIdentityInUpn(),
+            });
+
+            //Act
+            var userEmail = testController.GetRequestUserEmail();
+
+            //Assert
+            Assert.IsFalse(string.IsNullOrWhiteSpace(userEmail));
+        }
+
+        [Test]
+        public void Given_IdentityWithoutRmail_When_GetRequestUserEmail_With_InvalidClaim_Then_Exception()
+        {
+            //Arrange
+            var testController = CreateTestController(new ControllerContext()
+            {
+                HttpContext = CreateHttpContextWithoutIdentity(),
+            });
+
+            //Act
+            TestDelegate act = () => testController.GetRequestUserEmail();
+
+            //Assert
+            var exception = Assert.Throws<InvalidOperationException>(act);
+        }
+
+
         private TestController CreateTestController(ControllerContext controllerContext = null)
         {
             return new TestController(new Mock<ILogger<TestController>>().Object)
@@ -114,12 +162,47 @@ namespace ChustaSoft.Common.AspNet.UnitTest
             };
         }
 
-        private Microsoft.AspNetCore.Http.DefaultHttpContext CreateHttpContextWithAuthrizationHeader()
+        private DefaultHttpContext CreateHttpContextWithAuthrizationHeader()
         {
-            var httpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext();
+            var httpContext = new DefaultHttpContext();
             httpContext.Request.Headers["Authorization"] = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Imwzc1EtNTBjQ0g0eEJWWkxIVEd3blNSNzY4MCIsImtpZCI6Imwzc1EtNTBjQ0g0eEJWWkxIVEd3blNSNzY4MCJ9.eyJhdWQiOiJhcGk6Ly9iNjZjYjc5MS01NWY4LTQ4OGMtODFjNC0yNzM2MjJhODVkNWEiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC80NTMxZDk4NC03M2QwLTQ0MzktYTEwNi00YThhNDRkZGI4YzcvIiwiaWF0IjoxNjMzOTM4NDU2LCJuYmYiOjE2MzM5Mzg0NTYsImV4cCI6MTYzMzk0MjM1NiwiYWNyIjoiMSIsImFpbyI6IkFWUUFxLzhUQUFBQXk3akw3UE0wWGs4azVuSXhMV0ZiRGhoeFIrQ1hORm9BODQvQUVDT1hCVXRzL2l1TTNIQTI0RDl1V25pYVlRa3VzZGRoS2ROdEYyWDZwdnVQRDZoMEVmN3JBTk9SZGJlWG5UWDM2UmZ6cU1zPSIsImFtciI6WyJwd2QiLCJtZmEiXSwiYXBwaWQiOiI2OTI4NDNmOS00N2M2LTQwMTctYjYyNS1mYjJiOTBkZTE1ZDgiLCJhcHBpZGFjciI6IjEiLCJlbWFpbCI6Impvc2UuZWlndXJlbkBjb3RlY25hLmVzIiwiZmFtaWx5X25hbWUiOiJFaWd1cmVuIiwiZ2l2ZW5fbmFtZSI6Ikpvc2UiLCJpcGFkZHIiOiI5MC4xNzMuMTQ3LjEwOCIsIm5hbWUiOiJFaWd1cmVuIEpvc2UiLCJvaWQiOiJmZjg1NDUyZi00NjViLTQ1MzktYTA1Ni1mZDUxNmQ2MzVkZjUiLCJvbnByZW1fc2lkIjoiUy0xLTUtMjEtNjA2NzQ3MTQ1LTE5NTc5OTQ0ODgtODM5NTIyMTE1LTI4NzM3IiwicmgiOiIwLkFZRUFoTmt4UmRCek9VU2hCa3FLUk4yNHhfbERLR25HUnhkQXRpWDdLNURlRmRpQkFDSS4iLCJzY3AiOiJwYXJraW5nX2FwaV9hY2Nlc3MiLCJzaWQiOiJlOTU3YzEzOC00NmU4LTQwMjAtYjI3My1mM2Q0N2RiNTFjNjYiLCJzdWIiOiJSQkpLWlFPTDRuQjZCME5pSGVHblFlVFpVWkVNUTJTN01PMnpUYU1ISXpjIiwidGlkIjoiNDUzMWQ5ODQtNzNkMC00NDM5LWExMDYtNGE4YTQ0ZGRiOGM3IiwidW5pcXVlX25hbWUiOiJqb3NlLmVpZ3VyZW5AY290ZWNuYS5lcyIsInVwbiI6Impvc2UuZWlndXJlbkBjb3RlY25hLmVzIiwidXRpIjoiS2V6SWhjTkM5a2VILURiaTRPN0lBQSIsInZlciI6IjEuMCJ9.MIfNYBb1_--GgHZM7-9JFd6mUjhU_Os-3CU8HToFn8rJc8IzFS0BgfJMLTn730YVYNYSfwFr8JGt5ISHdHkorFJdAi7jY6n_hESCK0uyC1jcXjiKgeMOP-8OMZ2OXGHbu_-e0Qb4ujUGrdWygSKfyv6kWH5-weLZjWv8I2mN0cdmotbNDbygyI6GJa9kPEEoQNkzxxLu-9yzpk7HKIV9BXL3KV1-XErRoDhKFJiaxlQooKTlxzjqaGoUVu7gTKdXHmOXJBdObhJIaaKBVcaW66iYM391LlVLK2hIahJ6YHXoyiwmySWBObHyP0mmfNRv6hrst_l77y8ax71DtSNe8Q";
 
             return httpContext;
+        }
+
+        private DefaultHttpContext CreateHttpContextWithIdentity()
+        {
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "Name"),
+                new Claim(ClaimTypes.NameIdentifier, Guid.Parse("0bd1cd81-f260-4005-ad6e-3c6e8b9ef6f9").ToString()),
+                new Claim(ClaimTypes.Email, "mail@test.com")
+            }, "MockedIdentity"));
+
+            return new DefaultHttpContext() { User = user };
+        }
+
+        private DefaultHttpContext CreateHttpContextWithoutIdentity()
+        {
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "Name"),
+                new Claim(ClaimTypes.NameIdentifier, Guid.Parse("0bd1cd81-f260-4005-ad6e-3c6e8b9ef6f9").ToString())               
+            }, "MockedIdentity"));
+
+            return new DefaultHttpContext() { User = user };
+        }
+
+        private DefaultHttpContext CreateHttpContextWithIdentityInUpn()
+        {
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "Name"),
+                new Claim(ClaimTypes.NameIdentifier, Guid.Parse("0bd1cd81-f260-4005-ad6e-3c6e8b9ef6f9").ToString()),
+                new Claim("upn", "mail@test.com")
+            }, "MockedIdentity"));
+
+            return new DefaultHttpContext() { User = user };
         }
 
 
