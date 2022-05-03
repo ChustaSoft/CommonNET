88f7ec741352be852d212a596fd56d80ce5b3b00
Xelit3 <xavi_abelaira@outlook.com>
2022-05-03 15:30:35 +0200

    Tracking performance on MVC pages

---
+++ ChustaSoft.Common.AspNet/Filters/TrackPagePerformanceFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ChustaSoft.Common.Filters
+{
+    public class TrackPagePerformanceFilter : IPageFilter
+    {
+        
+        private readonly ITrackPagePerformanceService _trackPagePerformanceService;
+
+
+        public TrackPagePerformanceFilter(ITrackPagePerformanceService trackPagePerformanceService)
+        {
+            _trackPagePerformanceService = trackPagePerformanceService;
+        }
+
+
+        public void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+            => _trackPagePerformanceService.OnInit(context);        
+
+        public void OnPageHandlerExecuted(PageHandlerExecutedContext context)
+            => _trackPagePerformanceService.OnFinish(context);        
+
+        public void OnPageHandlerSelected(PageHandlerSelectedContext context) { }
+
+    }
+}
---
+++ ChustaSoft.Common.AspNet/Filters/TrackPagePerformanceService.cs
@@ -0,0 +1,44 @@
+﻿using ChustaSoft.Common.Helpers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace ChustaSoft.Common.Filters
+{
+
+    public interface ITrackPagePerformanceService
+    {
+        void OnInit(PageHandlerExecutingContext context);
+        void OnFinish(PageHandlerExecutedContext context);
+    }
+
+
+
+    public class TrackPagePerformanceService : ITrackPagePerformanceService
+    {
+
+        private readonly ILogger _logger;
+        private Stopwatch _timer;
+
+
+        public TrackPagePerformanceService(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger("TRACKING-PERFORMANCE");
+        }
+
+        public void OnInit(PageHandlerExecutingContext context)
+        {
+            _timer = Stopwatch.StartNew();
+        }
+
+        public void OnFinish(PageHandlerExecutedContext context)
+        {
+            _timer.Stop();
+            if (context.Exception == null)
+            {
+                _logger.LogPerformance(context, _timer);
+            }
+        }
+       
+    }
+}
