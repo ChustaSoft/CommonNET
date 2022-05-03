cba22c230dc3ab3629b38b0cb93c61ec89e8211e
Xelit3 <xavi_abelaira@outlook.com>
2022-05-03 15:31:04 +0200

    MVC extension methods for configuring new filters

---
+++ ChustaSoft.Common.AspNet/Extensions/MvcOptionsExtensions.cs
@@ -0,0 +1,19 @@
+﻿using ChustaSoft.Common.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChustaSoft.Common.Extensions
+{
+    public static class MvcOptionsExtensions
+    {
+
+        public static void AddActionPerformanceFilter(this MvcOptions mvcOptions)
+        {
+            mvcOptions.Filters.Add(typeof(TrackActionPerformanceFilter));
+        }
+
+        public static void AddPagePerformanceFilter(this MvcOptions mvcOptions)
+        {
+            mvcOptions.Filters.Add(typeof(TrackPagePerformanceFilter));
+        }
+    }
+}
