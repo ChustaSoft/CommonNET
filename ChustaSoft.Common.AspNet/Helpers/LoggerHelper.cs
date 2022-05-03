0ad6325628033fac17e8268914e03362ff462a61
Xelit3 <xavi_abelaira@outlook.com>
2022-05-03 15:30:49 +0200

    LoggerHelper for logger methods definition

---
+++ ChustaSoft.Common.AspNet/Helpers/LoggerHelper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+
+namespace ChustaSoft.Common.Helpers
+{
+    public static class LoggerHelper
+    {
+
+        private static readonly Action<ILogger, string, string, long, Exception> _logPerformanceAction;
+
+
+        static LoggerHelper()
+        {
+            _logPerformanceAction = LoggerMessage.Define<string, string, long>(LogLevel.Information, 0, "{Action}-{Method} took: {ElapsedMiliseconds}");
+        }
+
+
+        public static void LogPerformance(this ILogger logger, ActionExecutedContext actionContext, Stopwatch stopwatch)
+        {            
+            if (actionContext.Exception == null)
+            {
+                _logPerformanceAction(logger, actionContext.HttpContext.Request.Path, actionContext.HttpContext.Request.Method, stopwatch.ElapsedMilliseconds, null);
+            }
+        }
+
+        public static void LogPerformance(this ILogger logger, PageHandlerExecutedContext pageContext, Stopwatch stopwatch)
+        {
+            if (pageContext.Exception == null)
+            {
+                _logPerformanceAction(logger, pageContext.HttpContext.Request.Path, pageContext.HttpContext.Request.Method, stopwatch.ElapsedMilliseconds, null);
+            }
+        }
+
+    }
+}
